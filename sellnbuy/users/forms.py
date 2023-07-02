28eff9612267950e6202aea5ca1f91e902a68336
Artem Usachov <ar.usa4yov@yandex.ru>
2023-07-02 22:01:06 +0300

    registration is ready

--- sellnbuy/users/forms.py
+++ sellnbuy/users/forms.py
@@ -5,12 +5,13 @@ from django import forms
 User = get_user_model()
 
 
-class UserRegistration(UserCreationForm):
+class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
-    first_name = forms.CharField(max_length=50)
-    last_name = forms.CharField(max_length=50)
-    phone = forms.CharField(max_length=18)
+    first_name = forms.CharField(max_length=50, label="Имя")
+    last_name = forms.CharField(max_length=50, label="Фамилия")
+    phone = forms.CharField(max_length=18, label="Телефонный номер")
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'phone', 'password1', 'password2')
+        fields = ['username', 'first_name', 'last_name', 'email', 'phone', 'password1', 'password2']
+
--- sellnbuy/users/views.py
+++ sellnbuy/users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
 
 # Create your views here.
 
 
 def register(request):
-    return render(request, 'users/registration.html')
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+        return render(request, 'users/registration.html', {'form': form})
+    form = UserRegistrationForm()
+    return render(request, 'users/registration.html', {'form': form})
