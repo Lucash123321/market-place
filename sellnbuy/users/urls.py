e349cbf4bd11e058a0ffd939b55ec95138212a2e
Artem Usachov <ar.usa4yov@yandex.ru>
2023-07-06 01:59:08 +0300

    forgot password function was added

--- sellnbuy/users/urls.py
+++ sellnbuy/users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, LoginView
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, LoginView, \
+    LogoutView
 
 app_name = "users"
 
 urlpatterns = [
-    path('sign-up', views.SignUp.as_view(), name='sing-up'),
-    path('password-reset', PasswordResetView.as_view(), name='password-reset'),
+    path('sign-up', views.SignUp.as_view(), name='sign-up'),
+    path('password-reset', PasswordResetView.as_view(template_name="users/password-reset.html"), name='password-reset'),
     path('password-reset-done', PasswordResetDoneView.as_view(), name='password-reset-done'),
     path('password-reset-confirm', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
-    path('login', LoginView.as_view(template_name="users/login.html"), name='login')
+    path('login', LoginView.as_view(template_name="users/login.html"), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
     ]
